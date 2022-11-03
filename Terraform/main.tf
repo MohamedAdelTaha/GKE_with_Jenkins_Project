f9f9043a440e975e2d6ba8a667010647be960108
Mohamed Adel <mohamed.adel.saber.taha@gmail.com>
2022-11-03 16:08:38 +0200

    Testing The Terraform Section

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,12 @@
 module "infra_module" {
   source = "./Infrastructure"
+  projectName = var.project
 }
 
 module "bastion_module" {
   source = "./Bastion_VM"
+}
+
+module "gke_cluster_module" {
+  source = "./modules/GKE_Cluster"
 }
